de6a82f65da38492287971a89032f7233d0eded9
Joselyn Moncayo <joselynmoncayo1999@gmail.com>
2023-03-09 11:45:58 -0500

    correcciones

--- src/main/java/com/proyecto/web/controller/EmpleadoRestfulController.java
+++ src/main/java/com/proyecto/web/controller/EmpleadoRestfulController.java
@@ -1,9 +1,15 @@
 package com.proyecto.web.controller;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +20,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.proyecto.web.repository.model.Reserva;
+import com.proyecto.web.repository.model.Vehiculo;
 import com.proyecto.web.service.IClienteService;
 import com.proyecto.web.service.IGestorEmpleadoService;
+import com.proyecto.web.service.IReservaService;
+import com.proyecto.web.service.IVehiculoService;
 import com.proyecto.web.service.to.ClienteActualizarTO;
 import com.proyecto.web.service.to.ClienteTO;
+import com.proyecto.web.service.to.VehiculoReservadoTo;
 import com.proyecto.web.service.to.VehiculoTO;
 
 @RestController
@@ -28,10 +39,74 @@ public class EmpleadoRestfulController {
 
 	@Autowired
 	private IGestorEmpleadoService iGestorEmpleadoService;
-	
+
 	@Autowired
 	private IClienteService clienteService;
+
+	@Autowired
+	private IReservaService reservaService;
 	
+	@Autowired
+    private IVehiculoService vehiculoService;
+
+    @PutMapping(path="/vehiculos", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void insertarVehiculoN(@RequestBody VehiculoTO vehiculo) {
+        this.vehiculoService.insertar(vehiculo);
+    }
+
+    @PostMapping(path="/vehiculos", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void actualizarVehiculo(@RequestBody Vehiculo vehiculo) {
+        this.vehiculoService.actualizar(vehiculo);
+    }
+
+    @DeleteMapping(path = "/vehiculos/{id}")
+    public void eliminarVehiculo(@PathVariable("id") Integer id) {
+        this.vehiculoService.borrar(id);
+    }
+
+    @GetMapping(path = "/vehiculos/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Vehiculo> buscarVehiculo(@PathVariable("id") Integer id) {
+        return ResponseEntity.status(HttpStatus.OK).body(this.vehiculoService.buscar(id));
+    }
+
+    @GetMapping(path = "/vehiculos/marca/{marca}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<VehiculoTO>> buscarVehiculosPorMarca(@PathVariable("marca") String marca) {
+        List<VehiculoTO> vehiculoTos = this.vehiculoService.buscarVehiculosPorMarca(marca);
+        for (VehiculoTO vehiculoTo : vehiculoTos) {
+            Link myLink = linkTo(methodOn(EmpleadoRestfulController.class).buscarVehiculo(vehiculoTo.getId())).withSelfRel();
+            vehiculoTo.add(myLink);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(vehiculoTos);
+    }
+
+    @GetMapping(path = "/vehiculos/numero/{numero}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<VehiculoReservadoTo> buscarVehiculoPorNumeroDeReserva(@PathVariable("numero") String numero) {
+        VehiculoReservadoTo vehiculo = this.vehiculoService.buscarVehiculoPorNumeroDeReserva(numero);
+        vehiculo.add(linkTo(methodOn(EmpleadoRestfulController.class).buscarVehiculo(vehiculo.getVehiculoId())).withRel("vehiculo"));
+        return ResponseEntity.status(HttpStatus.OK).body(vehiculo);
+    }
+	
+	
+	//Reservas NICO
+
+	@GetMapping(path = "/reservas/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Reserva buscarReserva(@PathVariable("id") Integer id) {
+		return this.reservaService.buscar(id);
+	}
+
+	@PutMapping(path = "/reservas/{numeroReserva}")
+	public void actualizarReserva(@PathVariable("numeroReserva") String numeroReserva) {
+		
+		Reserva reserva = this.reservaService.buscarPorNumero(numeroReserva);
+		reserva.setEstado('E');
+		
+		Vehiculo v = reserva.getVehiculoReservado();
+		v.setEstado("ND");
+		reserva.setVehiculoReservado(v);
+		
+		this.reservaService.actualizar(reserva);
+	}
+
 	@PostMapping(path = "/clientes", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public void registrarCliente(@RequestBody ClienteTO cliente) {
 		this.clienteService.registrar(cliente);
@@ -68,7 +143,7 @@ public class EmpleadoRestfulController {
 	}
 
 	// Actualizar Cliente
-	@PutMapping(path="/clientes",consumes = { MediaType.APPLICATION_JSON_VALUE })
+	@PutMapping(path = "/clientes", consumes = { MediaType.APPLICATION_JSON_VALUE })
 	public String actualizarCliente(@RequestBody ClienteActualizarTO cliente) {
 		String msj = "Cliente actualizado correctamente";
 		try {
